a39c30631f3369354c96e0eb29245407f6f2ee1c
Lonnie VanZandt <lonniev@gmail.com>
2017-04-11 20:52:20 -0600

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,11 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+tomcat_install node['linux-tomcat']['instance_name'] do
+  version node['linux-tomcat']['version']
+end
+
+tomcat_service node['linux-tomcat']['instance_name'] do
+  action :start
+end
